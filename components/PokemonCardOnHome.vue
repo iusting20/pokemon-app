2773ada3fbae50b4884494b2e90aaa0d0d691aa9
Iustin Ghergu <iusting20@gmail.com>
2022-07-31 11:34:26 +0300

    now we have a list of pokemons on homepage

---
+++ components/PokemonCardOnHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ pk_name }}</div>
+</template>
+
+<script>
+export default {
+  props: ["pk_name"],
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  border: 3px solid blue;
+  margin-top: 2em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,27 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6"> </v-col>
+    <v-col cols="12" sm="8" md="6">
+      <div v-for="pokemon in pokemonList.results">
+        <PokemonCardOnHome :pk_name="pokemon.name"></PokemonCardOnHome>
+      </div>
+    </v-col>
   </v-row>
 </template>
 
 <script>
+import PokemonCardOnHome from "~/components/PokemonCardOnHome.vue";
 export default {
   name: "IndexPage",
   data() {
     return {
-      posts: "",
+      pokemonList: "",
     };
   },
   async fetch() {
-    this.posts = await this.$axios.$get("https://pokeapi.co/api/v2/pokemon/");
+    this.pokemonList = await this.$axios.$get(
+      "https://pokeapi.co/api/v2/pokemon/"
+    );
   },
+  components: { PokemonCardOnHome },
 };
 </script>
